2edbca7f581caded901eb5a248a0660d9f385ff8
julianchoi007 <choijulian007@gmail.com>
2024-11-23 22:24:02 -0500

    test stuff

--- meta/src/App.jsx
+++ meta/src/App.jsx
@@ -1,18 +1,23 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LandingPage } from './pages/LandingPage';
-import { SetupPage } from './pages/SetupPage';
-import TestPage from './pages/TestPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage";
+import { SetupPage } from "./pages/SetupPage";
+import { WebGazerProvider } from "./context/WebGazerContext";
+import TestPage from "./pages/TestPage";
+import { TextInputPage } from "./pages/TextInputPage";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/setup" element={<SetupPage />} />
-        <Route path="/test" element={<TestPage />} />
-      </Routes>
-    </Router>
+    <WebGazerProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/setup" element={<SetupPage />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/text" element={<TextInputPage />} />
+        </Routes>
+      </Router>
+    </WebGazerProvider>
   );
 };
 
